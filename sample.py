fef37232b55bbc83e17b48ea83d66ee918e1813d
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-12-02 17:50:51 +0900

    save

--- sample.py
+++ sample.py
@@ -13,3 +13,31 @@ inputs = processor(raw_image, input_points=input_points, return_tensors="pt").to
 outputs = model(**inputs)
 masks = processor.image_processor.post_process_masks(outputs.pred_masks.cpu(), inputs["original_sizes"].cpu(), inputs["reshaped_input_sizes"].cpu())
 scores = outputs.iou_scores
+
+from transformers import pipeline
+generator =  pipeline("mask-generation", device = -1, points_per_batch = 256)
+image_url = "https://huggingface.co/ybelkada/segment-anything/resolve/main/assets/car.png"
+outputs = generator(image_url, points_per_batch = 256)
+
+
+
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+
+plt.imshow(np.array(raw_image))
+ax = plt.gca()
+for mask in outputs["masks"]:
+    show_mask(mask, ax=ax, random_color=True)
+plt.axis("off")
+plt.show()
